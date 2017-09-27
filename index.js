311dd118126fb67acda31dc8d19a760f04458801
Siddhartha Nutulapati <sidgonuts@gmail.com>
2017-09-26 21:48:44 -0400

    scaffolding and im tired

--- index.js
+++ index.js
@@ -12,6 +12,17 @@ class ServerlessCustomDomain {
 
   alertUser() {
     // get config from serverless.yml
+    const service = this.serverless.service;
+    const serviceName = service.service;
+    const customVars = service.custom.serverlessDing;
+    console.log(serviceName);
+    console.log(customVars);
+    if (customVars.soundFailure) {
+
+    }
+    if (customVars.soundSuccess) {
+
+    }
     // at some point check for os type
     // display push notification
     // play appropriate sound (if success, happy.mp3 ; if failure, sad_trombone.mp3)
