f63da179a3046f92e116000cfa859ea89d263f24
Siddhartha Nutulapati <sidgonuts@gmail.com>
2017-11-05 08:34:36 -0500

    scaffolded, gotta make sure it works

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # serverless-ding
 
+# Why
+Because running `sls deploy` is just long enough to go do something else and forget that you ran `sls deploy`. Figured a notification would be nice.
+
 # Getting Started
 
 ## Prerequisites
@@ -7,3 +10,17 @@ Make sure you have the following installed before starting:
 * [nodejs](https://nodejs.org/en/download/)
 * [npm](https://www.npmjs.com/get-npm?utm_source=house&utm_medium=homepage&utm_campaign=free%20orgs&utm_term=Install%20npm)
 * [serverless](https://serverless.com/framework/docs/providers/aws/guide/installation/)
+
+# Things To Do Before We're Done Here
+ * Play different sounds on (un)successful deploy
+ * Let user choose what sounds to play
+ * Notifications from Terminal for macOS (like Chrome notifications)
+ * Support operating systems besides macOS
+ * Unit tests
+ * Linting
+ * Publish to npm
+ * Badges in the README
+
+# Attribution
+
+The sound effect `doorbell.wav` was taken from [here](https://freesound.org/people/Corsica_S/sounds/163730/)
--- index.js
+++ index.js
@@ -1,31 +1,34 @@
 'use strict';
 
+// const player = require('play-sound')();
+var Player = require('player'); // @TODO USE THIS!!!!
+const fs = require('fs');
+
 
 class ServerlessCustomDomain {
 
   constructor(serverless) {
     this.serverless = serverless;
+
+    // hooks tell when to run what function
     this.hooks = {
       'after:deploy:deploy': this.alertUser.bind(this),
     };
   }
 
+  /**
+   * Alerts the user with a doorbell noise
+   */
   alertUser() {
-    // get config from serverless.yml
-    const service = this.serverless.service;
-    const serviceName = service.service;
-    const customVars = service.custom.serverlessDing;
-    console.log(serviceName);
-    console.log(customVars);
-    if (customVars.soundFailure) {
-
+    if (fs.existsSync('doorbell.wav')) {
+        console.log('stuff exists!!!');
     }
-    if (customVars.soundSuccess) {
-
-    }
-    // at some point check for os type
-    // display push notification
-    // play appropriate sound (if success, happy.mp3 ; if failure, sad_trombone.mp3)
+    player.play('doorbell.wav', (err) => {
+      if (err) {
+        this.serverless.cli.consoleLog('Notifying user of deploy failed.');
+        console.log(err);
+      }
+    });
   }
 
 }
